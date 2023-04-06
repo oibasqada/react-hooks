dd4893d4fb10bfaf49a2d4265d29a8d09d871457
oibasqada <tumarbekovmax@gmail.com>
2023-04-06 16:27:27 +0600

    meal page structure done, no design

--- src/pages/meal-page.js
+++ src/pages/meal-page.js
@@ -33,6 +33,89 @@ const MealPage = () => {
                                 <img src={meals.strMealThumb} alt=""/>
                                 <p>{meals.strInstructions}</p>
                             </div>
+                            <h2>Ingredients:</h2>
+                            <div className="ingr-s">
+                                {
+
+                                    meals.strIngredient1 &&
+                                    <div className="misc">
+                                        <img src={`https://themealdb.com/images/ingredients/${meals.strIngredient1}.png`} alt="?"/>
+                                        <a href={'#'}>{meals.strIngredient1}</a>
+                                    </div>
+                                }
+                                {
+
+                                    meals.strIngredient2 &&
+                                    <div className="misc">
+                                        <img src={`https://themealdb.com/images/ingredients/${meals.strIngredient2}.png`} alt="?"/>
+                                        <a href={'#'}>{meals.strIngredient2}</a>
+                                    </div>
+                                }
+
+                                {
+                                    meals.strIngredient3 &&
+                                    <div className="misc">
+                                        <img src={`https://themealdb.com/images/ingredients/${meals.strIngredient3}.png`} alt="?"/>
+                                        <a href={'#'}>{meals.strIngredient3}</a>
+                                    </div>
+                                }
+
+                                {
+                                    meals.strIngredient4 &&
+                                    <div className="misc">
+                                        <img src={`https://themealdb.com/images/ingredients/${meals.strIngredient4}.png`} alt="?"/>
+                                        <a href={'#'}>{meals.strIngredient4}</a>
+                                    </div>
+                                }
+
+                                {
+                                    meals.strIngredient5 &&
+                                    <div className="misc">
+                                        <img src={`https://themealdb.com/images/ingredients/${meals.strIngredient5}.png`} alt="?"/>
+                                        <a href={'#'}>{meals.strIngredient5}</a>
+                                    </div>
+                                }
+
+                                {
+                                    meals.strIngredient6 &&
+                                    <div className="misc">
+                                        <img src={`https://themealdb.com/images/ingredients/${meals.strIngredient6}.png`} alt="?"/>
+                                        <a href={'#'}>{meals.strIngredient6}</a>
+                                    </div>
+                                }
+
+                                {
+                                    meals.strIngredient7 &&
+                                    <div className="misc">
+                                        <img src={`https://themealdb.com/images/ingredients/${meals.strIngredient7}.png`} alt="?"/>
+                                        <a href={'#'}>{meals.strIngredient7}</a>
+                                    </div>
+                                }
+
+                                {
+                                    meals.strIngredient8 &&
+                                    <div className="misc">
+                                        <img src={`https://themealdb.com/images/ingredients/${meals.strIngredient8}.png`} alt="?"/>
+                                        <a href={'#'}>{meals.strIngredient8}</a>
+                                    </div>
+                                }
+
+                                {
+                                    meals.strIngredient9 &&
+                                    <div className="misc">
+                                        <img src={`https://themealdb.com/images/ingredients/${meals.strIngredient9}.png`} alt="?"/>
+                                        <a href={'#'}>{meals.strIngredient9}</a>
+                                    </div>
+                                }
+
+                                {
+                                    meals.strIngredient10 &&
+                                    <div className="misc">
+                                        <img src={`https://themealdb.com/images/ingredients/${meals.strIngredient10}.png`} alt="?"/>
+                                        <a href={'#'}>{meals.strIngredient10}</a>
+                                    </div>
+                                }
+                            </div>
 
                         </div>
                 }
